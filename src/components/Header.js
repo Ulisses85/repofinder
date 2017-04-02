f5c355f5c45655469bb63f9f72553b4ec20797c7
Ulisses85 <lukasz.kuligowski@gmail.com>
2017-04-02 22:00:41 +0100

    Header component with image of github ninja

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = (props) => {
+  return (
+  <div className="row">
+    <img src="https://camo.githubusercontent.com/b5d1fce147445b2e54412caa5a2d86fc98a85fbb/68747470733a2f2f6f63746f6465782e6769746875622e636f6d2f696d616765732f646f6a6f6361742e6a7067" height="200px" className="center-block"></img>
+    </div>
+  )
+}
+
+export default Header;
