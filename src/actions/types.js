1dddc3108987e6080b9f607928935274972738b3
Ulisses85 <lukasz.kuligowski@gmail.com>
2017-04-02 20:29:16 +0100

    action types added

---
+++ src/actions/types.js
@@ -0,0 +1,4 @@
+export const SEARCH_START = 'SEARCH_START';
+export const SEARCH_PROCESS = 'SEARCH_PROCESS';
+export const SEARCH_FEEDBACK = 'SEARCH_FEEDBACK';
+export const SEARCH_END = 'SEARCH_END';
