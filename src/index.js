91d1ab8b38ea40223c5198040dfba33186fc0d99
Ulisses85 <lukasz.kuligowski@gmail.com>
2017-04-02 21:59:01 +0100

    index.js configured with necessary imports redux, css app and wrapped in the provider

--- src/index.js
+++ src/index.js
@@ -1,10 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+import createLogger from 'redux-logger';
 
+import '../public/style.css';
+import App from './components/App.js';
+import 'bootstrap/dist/css/bootstrap.css';
+import reducer from './reducers/reducers.js';
 
+const middleware = [ thunk ]
+if (process.env.NODE_ENV !== 'production') {
+  middleware.push(createLogger())
+}
+
+const store = createStore(
+  reducer,
+  applyMiddleware(...middleware)
+);
 
 ReactDOM.render(
-		<App/>,
-  	document.getElementById('root')
-);
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root'));
